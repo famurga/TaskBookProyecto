454205dcd6d5bc832d7707a87e19d1f67669961c
Frank <fmurga@unsa.edu.pe>
2020-08-08 02:53:35 -0500

    Commit PC 9

--- app/src/main/java/com/example/taskbookproyecto/Actividades.java
+++ app/src/main/java/com/example/taskbookproyecto/Actividades.java
@@ -2,8 +2,11 @@ package com.example.taskbookproyecto;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +14,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -27,6 +33,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Actividades extends Fragment {
 
@@ -41,6 +50,9 @@ public class Actividades extends Fragment {
         int imagen;
     FloatingActionButton floatingActionButton;
     public String personName;
+    ImageView micImageView;
+    private static final int SPEECH_REQUEST_CODE = 0;
+     EditText edtDescripcion;
 
 
 
@@ -54,7 +66,7 @@ public class Actividades extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_actividades, container, false);
+        final View view = inflater.inflate(R.layout.fragment_actividades, container, false);
         recyclerViewActividades = view.findViewById(R.id.recyclerView);
 
 
@@ -70,6 +82,38 @@ public class Actividades extends Fragment {
                  edtNombre = (EditText) mview.findViewById(R.id.edtNombreActividad);
 
                 edtFecha = (EditText) mview.findViewById(R.id.edtFechaActividad);
+                micImageView = mview.findViewById(R.id.microAlertDialog);
+
+                micImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        try{
+                            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+                            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+                        }
+                        catch(ActivityNotFoundException e)
+                        {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW,   Uri.parse("https://market.android.com/details?id=com.google.android.googlequicksearchbox"));
+                            startActivity(browserIntent);
+
+                        }
+
+
+
+                    }
+                });
+
+
+
+
+
+
+
                edtFecha.setOnClickListener(new View.OnClickListener() {
                    @Override
                    public void onClick(View v) {
@@ -78,7 +122,7 @@ public class Actividades extends Fragment {
 
                    }
                });
-                final EditText edtDescripcion = (EditText) mview.findViewById(R.id.edtDescripcionActividad);
+                 edtDescripcion = (EditText) mview.findViewById(R.id.edtDescripcionActividad);
                 Button btnCrearActividad = (Button) mview.findViewById(R.id.btnDialogCrear);
                 final Button btnCancelar = (Button) mview.findViewById(R.id.btnDialogCancelar);
 
@@ -123,6 +167,9 @@ public class Actividades extends Fragment {
                     }
                 });
 
+
+
+
                 mBuilder.setView(mview);
                 AlertDialog dialog =mBuilder.create();
                 dialog.show();
@@ -131,6 +178,7 @@ public class Actividades extends Fragment {
 
             }
         });
+
 
 
 
@@ -147,6 +195,56 @@ public class Actividades extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
+
+
+
+    // Cree una intención que pueda iniciar la actividad de Reconocimiento de voz
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Comience la actividad, la intención se completará con el texto del discurso
+       startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+
+    // Esta devolución de llamada se invoca cuando regresa el Reconocimiento de voz.
+    // Aquí es donde procesas la intención y extraes el texto del discurso de la intención.
+
+    public void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+            descripcion =spokenText;
+          //  edtDescripcion = (EditText) getActivity().findViewById(R.id.edtDescripcionActividad);
+            edtDescripcion.setText(descripcion);
+
+            /*
+            if(spokenText=="siguiente página"){
+                Intent intent1 = new Intent(this,ActivitySiguiente.class);
+                startActivity(intent1);
+            }
+            if(spokenText.equals("siguiente página")){
+                Intent intent1 = new Intent(this,ActivitySiguiente.class);
+                startActivity(intent1);
+            }
+
+            if(spokenText.equals("hola")){
+                Intent intent1 = new Intent(this,ActivitySiguiente.class);
+                startActivity(intent1);
+            }
+            */
+            // Haz algo con voiceText
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
 
     public void showDataPickerDialog(){
         //DataPickerFragment newFragment1 =  new DataPickerFragment();
