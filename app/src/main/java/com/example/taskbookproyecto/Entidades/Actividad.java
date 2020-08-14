f910f8d870e7728a89d557a299fb655d3352818e
Frank <fmurga@unsa.edu.pe>
2020-08-13 21:12:43 -0500

    Commit PC 15

--- app/src/main/java/com/example/taskbookproyecto/Entidades/Actividad.java
+++ app/src/main/java/com/example/taskbookproyecto/Entidades/Actividad.java
@@ -1,6 +1,8 @@
 package com.example.taskbookproyecto.Entidades;
 
-public class Actividad {
+import java.io.Serializable;
+
+public class Actividad implements Serializable {
     private String nombre;
     private String fecha;
     private int imagenId;
